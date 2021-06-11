a03c493ca3252f64dd3fb7695e5ea55dcba9bab7
Samarth Roy <samarthroy2106@gmail.com>
2021-06-11 20:00:19 +0530

    Wired the frontend to display errors and forecaast, finshed core app functionality

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,27 @@
 console.log('Client side js file is loaded!')
 
-fetch('http://localhost:3000/weather?address=Boston').then(response => {
-  response.json().then(data => {
-    if(data.error) {
-      console.log(data.error)
-    } else {
-      console.log(data.location)
-      console.log(data.forecast)
-    }
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const message1 = document.querySelector('#message-1')
+const message2 = document.querySelector('#message-2')
+
+weatherForm.addEventListener('submit', (event) => {
+  event.preventDefault()
+
+  const location = search.value
+
+  message1.textContent = 'Loading...'
+  message2.textContent = ''
+
+  fetch('http://localhost:3000/weather?address=' + location).then(response => {
+    response.json().then(data => {
+      if(data.error) {
+        message1.textContent = data.error
+        message2.textContent = ''
+      } else {
+        message1.textContent = data.location
+        message2.textContent = data.forecast
+      }
+    })
   })
 })
