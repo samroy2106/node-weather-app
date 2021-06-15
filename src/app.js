0ef08ab5e106b25755b0f4cd57b31bf6f0850e0f
Samarth Roy <samarthroy2106@gmail.com>
2021-06-15 21:54:48 +0530

    Fixed path variables

--- src/app.js
+++ src/app.js
@@ -8,9 +8,9 @@ const app = express()
 const port = process.env.PORT || 3000
 
 // Define paths for Express config
-const publicDirPath = path.join(__dirname, '..\\public')
-const viewsPath = path.join(__dirname, '..\\templates\\views')
-const partialsPath = path.join(__dirname, '..\\templates\\partials')
+const publicDirPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
 
 // Set up handlebars engine and views location
 app.set('view engine', 'hbs')
